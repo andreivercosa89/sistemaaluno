c049c79a4eaa736cb9fbf5ea18d8988fdd743e6f
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-10-30 21:44:24 -0400

    calcular media

---
+++ sistemaaluno/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace sistemaaluno
+{
+    public class Aluno
+    {
+        public string Nome{
+            set;get;
+        }
+
+        public double Nota1{
+            set; get;
+        }
+
+        public double Nota2
+        {
+            set; get;
+        }
+
+        public double Nota3
+        {
+            set; get;
+        }
+
+        public double Media(){
+            return Math.Round((Nota1 + Nota2 + Nota3) / 3, 2); ;
+        }
+
+    }
+}
---
+++ sistemaaluno/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace sistemaaluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.WriteLine("Informe o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("\nInforme a 1a nota: ");
+            aluno.Nota1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInforme a 2a nota: ");
+            aluno.Nota2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInforme a 3a nota: ");
+            aluno.Nota3 = Int32.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("A media do aluno {0} e: {1}", aluno.Nome, aluno.Media());
+        }
+    }
+}
